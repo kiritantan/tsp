a7bf8fe10cb28a2b6a23be824f536ba9c1f37510
kiri <conohatotama@google.com>
2016-04-23 02:25:18 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from citygenerator import CityGenerator
+from solveruser import SolverUser
+
+if __name__ == "__main__":
+    file_name = "city"
+    generator = CityGenerator()
+    generator.generate(file_name)
+
