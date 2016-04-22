7fe648f6fe669551ade0d2e163d2faaa9611d624
kiri <conohatotama@google.com>
2016-04-22 19:30:40 +0900

    add cityGenerator.py

---
+++ cityGenerator.py
@@ -0,0 +1,15 @@
+import random
+
+if __name__ == "__main__":
+    # 街と街が被らないように生成する(dictを用いる?)
+    city_dict = {}
+    count = 0
+    while len(city_dict) < 100:
+        x = random.randrange(100)
+        y = random.randrange(100)
+        if not "{0},{1}".format(x,y) in city_dict :
+            print("{0},{1}".format(x, y))
+            city_dict["{0},{1}".format(x,y)] = True
+        count += 1
+
+    print(count)
