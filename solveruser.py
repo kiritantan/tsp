2fee929f2fbb8f70a337bc90de169b70fadb5e52
kiri <conohatotama@google.com>
2016-04-23 16:33:47 +0900

    add solver : Counting

--- solveruser.py
+++ solveruser.py
@@ -1,10 +1,13 @@
 from basesolver import BaseSolver
+from counting import Counting
 
 
 class SolverUser():
     def __init__(self, solver_name="BaseSolver"):
         if solver_name == "BaseSolver":
             self.solver = BaseSolver()
+        if solver_name == "Counting":
+            self.solver = Counting()
 
     # 問題を解き、結果を最短系で都市をつなげたグラフで表示する
     def solve_with_graph(self, city_list):
@@ -20,4 +23,4 @@ class SolverUser():
 
     # 問題を解く
     def __solve(self, city_list):
-        self.solver.solve(city_list)
+        return self.solver.solve(city_list)
