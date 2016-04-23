e793a4460b51ebfda2490e8f19c575a942201dde
kiri <conohatotama@google.com>
2016-04-23 12:38:13 +0900

    add reference to parser

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 from citygenerator import CityGenerator
+from parser import  Parser
 from solveruser import SolverUser
 
 if __name__ == "__main__":
     file_name = "city"
     generator = CityGenerator()
     generator.generate(file_name)
-
+    parser = Parser()
+    print(parser.parse(file_name))
+    user = SolverUser()
+    user.solve_with_cost()
