5f1c15575ca3d7b3019097243367b37dd473179c
kiri <conohatotama@google.com>
2016-04-23 19:57:46 +0900

    narrow scale of city

--- citygenerator.py
+++ citygenerator.py
@@ -9,8 +9,8 @@ class CityGenerator():
         loop_number = 10
         f = open(self.file_name, 'w')
         while len(city_dict) < loop_number:
-            x = round(random.random() * (10 ** 5), 4)
-            y = round(random.random() * (10 ** 5), 4)
+            x = round(random.random() * (10 ** 3), 4)
+            y = round(random.random() * (10 ** 3), 4)
             if not "{0},{1}".format(x, y) in city_dict:
                 f.write("{0} {1} {2}\n".format(len(city_dict), x, y))
                 city_dict["{0},{1}".format(x, y)] = True
