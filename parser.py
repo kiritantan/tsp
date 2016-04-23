e23da6336a0399ebb9fc9555c41db5b9342c25b0
kiri <conohatotama@google.com>
2016-04-23 15:12:44 +0900

    fix typo, change element's type of list str to float

--- parser.py
+++ parser.py
@@ -3,9 +3,9 @@ class Parser():
     def parse(self,file_name="city"):
         file = open(file_name+".ssv")
         lines = file.readlines()
-        city_coodinates = []
+        city_coordinates = []
         for line in lines:
-            coodinates = line.strip().split(" ")[1:]
-            city_coodinates.append(coodinates)
-
-        return city_coodinates
+            list = line.strip().split(" ")[1:]
+            coordinates = [float(x) for x in list]
+            city_coordinates.append(coordinates)
+        return city_coordinates
