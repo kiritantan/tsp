7f93252cd20a6fc725f019173abd2cdcab4e5146
kiri <conohatotama@google.com>
2016-04-23 15:13:27 +0900

    add create_matrix method

--- basesolver.py
+++ basesolver.py
@@ -2,10 +2,20 @@ from math import sqrt
 
 
 class BaseSolver(object):
-    def solve(self,city_list=[],isVisualized=False):
+
+    def solve(self, city_list, isVisualized=False):
         raise Exception("plese set solve algorithm")
 
-    def disc(self, city1, city2):
+    def __create_matrix(self, city_list):
+        dist_matrix = []
+        for i in range(0, len(city_list)):
+            dist_list = []
+            for j in range(0, len(city_list)):
+                dist_list.append(self.__disc(city_list[i],city_list[j]))
+            dist_matrix.append(dist_list)
+        return dist_matrix
+
+    def __disc(self, city1, city2):
         dx = city1[0]-city2[0]
         dy = city1[1]-city2[1]
-        return int(sqrt(dx**2+dx**2)+0.5)
+        return int(sqrt(dx**2+dy**2)+0.5)
