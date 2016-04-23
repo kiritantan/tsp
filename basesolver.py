d34afc40e4590114186e499433b9cc2c1979fbab
kiri <conohatotama@google.com>
2016-04-23 17:58:47 +0900

    change private method to public method

--- basesolver.py
+++ basesolver.py
@@ -11,7 +11,7 @@ class BaseSolver(object):
     def solve(self, city_list, isVisualized=False):
         raise Exception("plese set solve algorithm")
 
-    def __create_matrix(self, city_list):
+    def create_matrix(self, city_list):
         dist_matrix = []
         for i in range(0, len(city_list)):
             dist_list = []
@@ -20,7 +20,7 @@ class BaseSolver(object):
             dist_matrix.append(dist_list)
         return dist_matrix
 
-    def __disc(self, city1, city2):
+    def disc(self, city1, city2):
         dx = city1[0]-city2[0]
         dy = city1[1]-city2[1]
         return int(sqrt(dx**2+dy**2)+0.5)
