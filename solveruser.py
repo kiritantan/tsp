465b190db933d71625239be82879c52e9bcfa220
kiri <conohatotama@google.com>
2016-04-23 12:37:20 +0900

    add solver

--- solveruser.py
+++ solveruser.py
@@ -1,8 +1,10 @@
+from basesolver import BaseSolver
+
 
 class SolverUser():
-    def __init__(self, solver_name="hoge"):
-        if solver_name == "hoge":
-            self.solver = "hoge"
+    def __init__(self, solver_name="BaseSolver"):
+        if solver_name == "BaseSolver":
+            self.solver = BaseSolver()
 
     # 問題を解き、結果を最短系で都市をつなげたグラフで表示する
     def solve_with_graph(self, city_list):
@@ -10,7 +12,7 @@ class SolverUser():
 
     # 問題を解き、結果を最短経路のリストで表示する
     def solve_with_list(self, city_list):
-        pass
+        return self.__solve(city_list)
 
     # 問題を解き、結果を最短経路のコストで表示する
     def solve_with_cost(self, city_list):
@@ -18,4 +20,4 @@ class SolverUser():
 
     # 問題を解く
     def __solve(self, city_list):
-        pass
+        self.solver.solve(self, city_list)
