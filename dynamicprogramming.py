650b8b1661611a314a6d5ab2845d726ac0f95f95
kiri <conohatotama@google.com>
2016-04-27 16:11:25 +0900

    enable to get route

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -1,46 +1,42 @@
 from basesolver import BaseSolver
 import sys
 
+#0を起点に路を返すようにする。
 class DynamicProgramming(BaseSolver):
-    def __init__(self):
-        self.city_set_matrix = []
-        self.SMAX = 0
-        self.city_number = 0
-        super().__init__()
 
     def solve(self, city_list):
-        self.city_number = len(city_list)
-        self.SMAX = 1 << self.city_number
-        self.city_set_matrix = [[sys.maxsize for _ in range(self.SMAX)] for _ in range(self.city_number)]
+        city_size = len(city_list)
+        SMAX = 1 << city_size
+        city_cost_matrix = [[[sys.maxsize, 0] for _ in range(SMAX)] for _ in range(city_size)]
 
-        for i in range(self.city_number):
-            self.city_set_matrix[i][0] = 0
+        for i in range(city_size-1):
+            city_cost_matrix[i][1 << i][0] = super().disc(city_list[i], city_list[city_size-1])
+            city_cost_matrix[i][1 << i][1] = city_size - 1
 
-        for i in range(self.city_number-1):
-            self.city_set_matrix[i][1 << i] = super().disc(city_list[i], city_list[self.city_number-1])
-
-        for s in range(1, self.SMAX):
-            for i in range(self.city_number):
+        for s in range(1, SMAX):
+            for i in range(city_size):
                 if not ((1 << i) & s): continue
-                for j in range(self.city_number):
+                for j in range(city_size):
                     if ((1 << j) & s): continue
-                    tmp = self.city_set_matrix[i][s] + super().disc(city_list[i], city_list[j])
-                    if tmp < self.city_set_matrix[j][s | (1 << j)]:
-                        self.city_set_matrix[j][s | (1 << j)] = tmp
+                    tmp = city_cost_matrix[i][s][0] + super().disc(city_list[i], city_list[j])
+                    if tmp < city_cost_matrix[j][s | (1 << j)][0]:
+                        city_cost_matrix[j][s | (1 << j)][0] = tmp
+                        city_cost_matrix[j][s | (1 << j)][1] = i
 
-        return ((self.city_set_matrix[self.city_number - 1][self.SMAX - 1]), )
+        return (self.get_route(city_cost_matrix, city_size, city_size - 1), city_cost_matrix[city_size - 1][SMAX - 1][0])
 
-#    def get_route(self, city_cost_matrix, city_number, start):
-#        route = [start + 1]
-#        v = 1 << start
-#        p = start
+    def get_route(self, city_cost_matrix, city_number, start):
+        route = [start]
+        v = (1 << city_number) - 1
+        p = start
 
-#        while len(route) < city_number:
-#            (_, q) = city_cost_matrix[v][p]
-#            route.append(q+1)
-#            v |= (1 << a)
-#            p = q
+        while len(route) < city_number:
+            q = city_cost_matrix[p][v][1]
+            route.append(q)
+            v ^= (1 << p)
+            p = q
 
-#        route.append(start+1)
-#        return route
+        route.append(start)
+
+        return route
 
