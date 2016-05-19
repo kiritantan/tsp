126fb062ce71cabf3c14c40ebe642e05c463752b
kiri <conohatotama@google.com>
2016-05-19 09:15:06 +0900

    add city_size arg in city_generator

--- citygenerator.py
+++ citygenerator.py
@@ -2,14 +2,14 @@ import random
 
 
 class CityGenerator():
-    def generate(self, name='city', city_number=10, random_seed=1):
+    def generate(self, name='city', city_number=10, city_size=1000, random_seed=1):
         self.file_name = name + '.ssv'
         random.seed(random_seed)
         city_dict = {}
         f = open(self.file_name, 'w')
         while len(city_dict) < city_number:
-            x = round(random.random() * (10 ** 3), 4)
-            y = round(random.random() * (10 ** 3), 4)
+            x = round(random.random() * (city_size), 0)
+            y = round(random.random() * (city_size), 0)
             if not "{0},{1}".format(x, y) in city_dict:
                 f.write("{0} {1} {2}\n".format(len(city_dict), x, y))
                 city_dict["{0},{1}".format(x, y)] = True
