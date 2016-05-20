40bf53e31ac940dc226ee3c49d16cf8e098a83e1
kiri <conohatotama@google.com>
2016-05-20 11:15:05 +0900

    adjust parse method to tsplib data\n(which small parsed)

--- parser.py
+++ parser.py
@@ -1,12 +1,15 @@
-
+import re
 
 class Parser():
     def parse(self,file_name="city"):
         file = open(file_name+".ssv")
         lines = file.readlines()
         city_coordinates = []
+        pattern = r'[ ]+'
+        repatter = re.compile(pattern)
         for line in lines:
-            list = line.strip().split(" ")[1:]
-            coordinates = [float(x) for x in list]
+            re_line = re.split(repatter, line)
+            fi_line = list(filter(lambda s: s != '', re_line))[1:]
+            coordinates = [float(x) for x in fi_line]
             city_coordinates.append(coordinates)
         return city_coordinates
