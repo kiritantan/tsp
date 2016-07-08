22324e8e6e99154f0c128e3325e2f88897ebdc0e
kiri <conohatotama@google.com>
2016-07-08 14:43:11 +0900

    rename firefly.py to fireflysystem.py

---
+++ solver/fireflysystem.py
@@ -0,0 +1,17 @@
+from solver.basesolver import BaseSolver
+
+
+class FireFly(BaseSolver):
+
+    def __init__(self):
+        super().__init__()
+        self.FIRE_FLY_NUMBER = 30
+
+    #15~40匹が良いらしい
+    #明るさ（コストの小さい順序）
+
+    def solve(self, city_list):
+        pass
+
+
+class Fire
