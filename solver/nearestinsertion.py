48a8884f924cb8b87e89ff217b72a6eff2b760ac
kiri <conohatotama@google.com>
2016-07-12 00:48:01 +0900

    rename nearestneighbor to nearestinsertion

---
+++ solver/nearestinsertion.py
@@ -0,0 +1,47 @@
+from solver.basesolver import BaseSolver
+
+
+class NearestNeighbor(BaseSolver):
+
+    def solve(self, city_list):
+        start = self.start_time()
+        initial_city = 0
+        city_number = len(city_list)
+        current_city = initial_city
+        cost_matrix = self.create_cost_matrix(city_list)
+        route = [current_city, current_city]
+        available_cities = [i for i in range(city_number)]
+        available_cities.remove(initial_city)
+        while(len(route) < len(city_list)):
+            nearest_city = -1
+            for souce in route[:-1]:
+                nearest_city_at_souce = available_cities[0]
+                for dist in available_cities[1:]:
+                    if cost_matrix[souce][nearest_city_at_souce] > cost_matrix[souce][dist]:
+                        nearest_city_at_souce = dist
+                if nearest_city == -1:
+                    nearest_city = nearest_city_at_souce
+                else:
+                    if nearest_city > nearest_city_at_souce:
+                        nearest_city = nearest_city_at_souce
+
+            nearest_insert_index = 0
+            for index in range(len(route)-1):
+                if cost_matrix[nearest_insert_index][nearest_city] + cost_matrix[nearest_insert_index][nearest_city] > \
+                        cost_matrix[index][nearest_city] + cost_matrix[index][nearest_city]:
+                    nearest_insert_index = index
+
+            route = route[:nearest_insert_index+1] + [nearest_city] + route[nearest_insert_index+1:]
+            available_cities.remove(nearest_city)
+        cost = self.cost_evaluate(route, city_list)
+        end = self.end_time(start)
+        return (route, cost, end)
+
+    def create_cost_matrix(self, city_list):
+        city_number = len(city_list)
+        route_cost_matrix = [[0 for _ in range(city_number)] for _ in range(city_number)]
+        for src_city_coordinate in range(city_number):
+            for dist_city_coodinate in range(city_number):
+                route_cost_matrix[src_city_coordinate][dist_city_coodinate] = self.disc(city_list[src_city_coordinate], \
+                                                                                    city_list[dist_city_coodinate])
+        return route_cost_matrix
